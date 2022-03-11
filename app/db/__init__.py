617e3eeab7c96d57c5790b7e3a4e96b52f366f6c
Hju Kneyck Flores <hjucode@gmail.com>
2022-03-11 20:30:11 +0800

    docs: remove extra description

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -4,10 +4,6 @@ app.db
 
 This module contains the operations for creating the database and its tables.
 
-We use SQLModel to create the database and its tables.
-
-Connecting to a database using a connection pool:
 1. We initialize connection pool using sqlmodel's `create_async_engine()`.
 2. A function yields a connection/session from the pool.
-3. Services use the function as a dependency (DI).
 """
