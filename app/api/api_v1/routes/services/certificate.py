2814723c41c71963afa5085025cc2acb633958ad
Hju Kneyck Flores <hjucode@gmail.com>
2022-10-01 12:02:21 +0800

    refactor: move part of `generate_certificate()` to internal fn

--- app/api/api_v1/routes/services/certificate.py
+++ app/api/api_v1/routes/services/certificate.py
@@ -12,28 +12,15 @@ from app.services import object_processor
 
 
 class CertificateService:  # pylint: disable=R0903
-    async def generate_certificate(  # pylint: disable=R0913
+    async def _generate_certificate(  # pylint: disable=R0913
         self,
         collections_schema: type[certificates.Certificates],
-        config_repo: config_repo_.ConfigurationsRepository,
-        config_service: configuration.ConfigurationService,
-        configs_schema: type[configurations.Configurations],
         data: certificate.CertificateTemplateMeta,
         database: crud.DatabaseImpl,
-        engine: sqlalchemy_asyncio.AsyncEngine,
-        fonts_schema: type[fonts.Fonts],
         object_processor_: object_processor.ObjectProcessor,
-        templates_schema: type[templates.Templates],
+        template_config: dict[str, typing.Any],
         request_id: uuid.UUID,
-    ) -> tuple[dict[str, typing.Any], int]:
-        template_config = await config_service.get_template_config(
-            template_config_id=data.template_config_id,
-            configs_schema=configs_schema,
-            templates_schema=templates_schema,
-            fonts_schema=fonts_schema,
-            database=config_repo,
-            engine=engine,
-        )
+    ):
         conf_ = template_config["template_config"]["config_meta"]
         result = await object_processor_.generate_certificate(
             certificate_meta={
@@ -59,3 +46,35 @@ class CertificateService:  # pylint: disable=R0903
         content["template_config_id"] = data.template_config_id
 
         return content, result[1]
+
+    async def generate_certificate(  # pylint: disable=R0913
+        self,
+        collections_schema: type[certificates.Certificates],
+        config_repo: config_repo_.ConfigurationsRepository,
+        config_service: configuration.ConfigurationService,
+        configs_schema: type[configurations.Configurations],
+        data: certificate.CertificateTemplateMeta,
+        database: crud.DatabaseImpl,
+        engine: sqlalchemy_asyncio.AsyncEngine,
+        fonts_schema: type[fonts.Fonts],
+        object_processor_: object_processor.ObjectProcessor,
+        templates_schema: type[templates.Templates],
+        request_id: uuid.UUID,
+    ) -> tuple[dict[str, typing.Any], int]:
+        template_config = await config_service.get_template_config(
+            template_config_id=data.template_config_id,
+            configs_schema=configs_schema,
+            templates_schema=templates_schema,
+            fonts_schema=fonts_schema,
+            database=config_repo,
+            engine=engine,
+        )
+
+        return await self._generate_certificate(
+            collections_schema=collections_schema,
+            data=data,
+            database=database,
+            object_processor_=object_processor_,
+            template_config=template_config,
+            request_id=request_id,
+        )
