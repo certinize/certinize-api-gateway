a5fa40f03edf211e912accdf47a3738c4c4cb088
Hju Kneyck Flores <hjucode@gmail.com>
2022-11-22 02:14:21 +0800

    fix: minor bug fixes

--- app/api/api_v1/routes/services/issuance.py
+++ app/api/api_v1/routes/services/issuance.py
@@ -1,6 +1,8 @@
 # pylint: disable=R0903
 import typing
 
+import starlite
+
 from app.db import crud
 from app.models.domain import issuance
 from app.models.schemas import users
@@ -15,8 +17,16 @@ class IssuanceService:
         blockchain_api_: blockchain_api.BlockchainInterface,
     ) -> dict[str, str]:
         user = await database.select(users.SolanaUsers(), "pubkey", public_key)
+        pvtkey = user.one().pvtkey
+
+        if pvtkey is None:
+            raise starlite.HTTPException(
+                status_code=400,
+                detail="User is not verified",
+            )
+
         return await blockchain_api_.get_unsigned_msg(
-            {"pubkey": public_key, "pvtkey": user.one().pvtkey}
+            {"pubkey": public_key, "pvtkey": pvtkey}
         )
 
     @staticmethod
